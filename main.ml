9d9a8392fd4662a9e7ba336342a49db477341c50
John Griffin <griffinish@gmail.com>
2026-06-18 09:52:55 -0400

    curses tweaking

--- main.ml
+++ main.ml
@@ -3,16 +3,28 @@ open Curses
 open Ropes
 open Model
 
+let update_windows (wins:window list) : unit =
+  let rec loop wins =
+    match wins with
+    | [] -> ()
+    | win::tail ->
+       ignore(wnoutrefresh win);
+       loop tail
+  in
+  loop wins
 
-let main w raw_bytes : unit =
+let main raw_bytes : unit =
+  let edit_win = newwin 30 60 10 10 in
   let text = Ustring.of_string raw_bytes in
   let multiple_texts = Ustring.split_with_nl text in
   let leaves = List.map (fun text -> Rope.Line text) multiple_texts in 
   let r = Ropes.Rope.build_rope leaves in
   let e:Model.t = { rope=r; loc=Rope.location_of 0 0; top_offset = 0 } in
+  let windows = [ stdscr (); edit_win ] in
   let rec loop e : unit =
-    let top_offset = View.view w e in
+    let top_offset = View.view edit_win e in
     let e = { e with top_offset } in
+    ignore(update_windows windows);
     ignore(refresh());
     let ch = getch() in
     match keyname(ch) with
@@ -37,7 +49,7 @@ let () =
   ignore(noecho());
   ignore(cbreak());
   ignore(curs_set 1);
-  main win raw_bytes;
+  main raw_bytes;
   ignore(endwin())
 
 
--- view.ml
+++ view.ml
@@ -40,7 +40,7 @@ let get_desired_top_lines w (e:Model.t) : int =
 
 
 let view w (e:Model.t) : int =
-  ignore(wclear w);
+  ignore(werase w);
   let active_ndx = e.loc.line_offset in
   let line_count = Rope.line_count e.rope in
   let screen_lines,screen_cols = getmaxyx w in
@@ -49,7 +49,7 @@ let view w (e:Model.t) : int =
   let cursor_x = ref 0 in
   let cursor_y = ref 0 in
   let rec loop screen_line ndx : unit =
-    if screen_line < screen_lines-4 && ndx < line_count then (
+    if screen_line < screen_lines-1 && ndx < line_count then (
       let loc = Model.location_of ndx 0 in
       let utext = Rope.line_at e.rope loc |> without_nl in
       let stext = Ustring.string_of utext in
@@ -58,9 +58,10 @@ let view w (e:Model.t) : int =
         let subtext = Ustring.sub utext 0 e.loc.char_offset |> Ustring.string_of in
         ignore(waddstr w subtext);
         let y,x = getyx w in
-        cursor_x := x;
-        cursor_y := y;
-        );
+        let offset_y, offset_x = getbegyx w in
+        cursor_x := x + offset_x;
+        cursor_y := y + offset_y;
+      );
       ignore(wmove w screen_line 0);
       ignore(waddstr w stext);
       let y,_ = getyx w in
@@ -69,8 +70,8 @@ let view w (e:Model.t) : int =
   in
   loop 0 top_ndx;
   ignore(wmove w 0 (screen_cols-20));
-  ignore(waddstr w (sprintf "[%d,%d,%d,%d]" e.loc.line_offset e.loc.char_offset desired_top_lines e.top_offset));
-  ignore(wnoutrefresh w);
+  ignore(waddstr w (sprintf "[%d,%d,%d,%d]"
+                      e.loc.line_offset e.loc.char_offset desired_top_lines e.top_offset));
   ignore(move !cursor_y !cursor_x);
   desired_top_lines
 
